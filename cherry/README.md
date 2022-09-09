99fa466e4ba0e86697cafcb0f08ee2bb68069c4b
fuu <fu@yihaofang.com>
2022-09-09 21:00:02 +0800

    Cherry editor  bug fix.

--- cherry/README.md
+++ cherry/README.md
@@ -2,4 +2,27 @@
 
 Editor bases on Cherry.
 
+- App protocol
+  
+```JSON
+{
+    "name":"che",
+    "protocol":{"type":"app","lib":["cherry"]}
+}
+```
 
+- Cherry library
+  
+```JSON
+{
+    "name":"cherry",
+    "protocol":{"type":"data","format":"JS","lib":["cherry_css"]}
+}
+```
+
+```JSON
+{
+    "name":"cherry_css",
+    "protocol":{"type":"data","format":"CSS"}
+}
+```
--- cherry/cherry.js
+++ cherry/cherry.js
@@ -2,28 +2,41 @@
 //1.使用new Function的方式加载cApp，const anchorApp=new Function("agent", "con", str);
 //2.传入3个参数 agent:polkadot处理网络的各种方法; con:dom容器的id ; jquery:jquery操作库
 
-//console.log(Arguments);
-const container = con;
-const tools = agent.tools;
+console.log(agent);
+console.log(con);
+console.log(error);
 
-const hash = function(n) { return Math.random().toString(36).substr(n != undefined ? n : 6) };
-
-
-const config={
-    app:'editor',   //设置筛选的data内容
-    cacheMax:10,    //cache的最大长度
-    cls:{
-        page:'w'+hash(),      //page容器的class
-    }
-};
-
-if($===undefined) console.log('No jquery exsist, cEditor will not run properly.');
 let editor=null;
-
-const self={
-    load:function(){
-
+const self = {
+    load: function () {
+        var cfg = {
+            id: con,
+            toolbars: {
+                toolbar: [
+                    'switchModel',
+                    '|',
+                    'bold',
+                    'italic',
+                    'strikethrough',
+                    '|',
+                    'color',
+                    'header',
+                    '|',
+                    'list',
+                    {
+                        insert: ['image', 'audio', 'video', 'link', 'hr', 'br', 'code', 'formula', 'toc', 'table', 'pdf', 'word'],
+                    },
+                    //'graph',
+                    //'settings',
+                ],
+            },
+            editor: {
+                defaultModel: 'editOnly',
+            },
+        }
+        editor = new Cherry(cfg);
+        console.log(editor);
     },
 }
 
-self.load();  //自动加载部分
+self.load();
